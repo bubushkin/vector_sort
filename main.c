c2525da8c082519e071a2713ba58f13f0c08eb2c
bubushkin <bubushkin@users.noreply.github.com>
2018-11-07 15:48:21 -0500

    implemented qs

--- main.c
+++ main.c
@@ -13,68 +13,91 @@
 #include "udef.h"
 #include "sorter.h"
 
-int n = 100;
-int m = 5;
+int vector_len = 1000;
+int cell_len = 5;
 
 int main(int argc, char **argv){
 
-	Sorter *p_merge_sort = init_sorter(0x1);
-	Vector **p_list_in = init_vector(n, m);
+	Sorter *p_sorter = init_sorter();
+	Vector **p_list_in = init_vector(vector_len, cell_len);
 
+#ifdef DEBUG
 	printf("Original\n");
 
-	for(int idx = 0x0; idx < n; idx++){
+	for(int idx = 0x0; idx < vector_len; idx++){
 		Vector *p_aux = p_list_in[idx];
-		for(int i = 0; i < m; i++)
-			printf("[%d] ", p_aux->pdata[i]);
+		printf("[ ");
+		for(int i = 0; i < cell_len; i++)
+			printf(" %d ", p_aux->pdata[i]);
+		printf(" ]");
 		printf("\n");
 	}
 	printf("\n");
+#endif
+	Vector **p_list_out = DMALLOC(Vector, vector_len);
 
-	Vector **p_list_out = DMALLOC(Vector, n);
+	t_start(&p_sorter->p_counter->timer);
+	p_sorter->p_merge_sort(p_sorter, p_list_in, p_list_out, 0x0, vector_len-1);
+	t_end(&p_sorter->p_counter->timer);
 
-	t_start(&p_merge_sort->p_counter->timer);
-	p_merge_sort->p_sort(p_merge_sort, p_list_in, p_list_out, 0x0, n-1);
-	t_end(&p_merge_sort->p_counter->timer);
-
+#ifdef DEBUG
 	printf("Sorted(MergeSort)\n");
-	for(int idx = 0x0; idx < n; idx++){
+	for(int idx = 0x0; idx < vector_len; idx++){
 		Vector *p_in = p_list_in[idx];
-		for(int i = 0; i < m; i++)
-			printf("[%d] ", p_in->pdata[i]);
+		printf("[ ");
+		for(int i = 0; i < cell_len; i++)
+			printf(" %d ", p_in->pdata[i]);
+		printf(" ]");
 		printf("\n");
 	}
-	t_print(&p_merge_sort->p_counter->timer, "Time took for MergeSort");
+#endif
 
+	t_print(&p_sorter->p_counter->timer, "Time took for MergeSort");
+	printf("Number of comparisions: %ld \n", p_sorter->p_counter->counter);
 
-	for(int idx = 0x0; idx < n; idx++){
+	del_vector(p_list_in, vector_len);
+	free(p_list_out);
+
+	/*
+	 * Quick-Sort
+	 */
+	p_list_in = init_vector(vector_len, cell_len);
+
+#ifdef DEBUG
+	printf("Original\n");
+
+	for(int idx = 0x0; idx < vector_len; idx++){
 		Vector *p_aux = p_list_in[idx];
-		p_aux->p_reverse(p_aux, m);
-	}
-
-	printf("Revrsed\n");
-
-	for(int idx = 0x0; idx < n; idx++){
-		Vector *p_aux = p_list_in[idx];
-		for(int i = 0; i < m; i++)
-			printf("[%d] ", p_aux->pdata[i]);
+		printf("[ ");
+		for(int i = 0; i < cell_len; i++)
+			printf(" %d ", p_aux->pdata[i]);
+		printf(" ]");
 		printf("\n");
 	}
 	printf("\n");
+#endif
 
-	t_start(&p_merge_sort->p_counter->timer);
-	p_merge_sort->p_sort(p_merge_sort, p_list_in, p_list_out, 0x0, n-1);
-	t_end(&p_merge_sort->p_counter->timer);
+	t_start(&p_sorter->p_counter->timer);
+	p_sorter->p_quick_sort(p_sorter, p_list_in, 0x0, vector_len - 1);
+	t_end(&p_sorter->p_counter->timer);
 
-	printf("Sorted(MergeSort)\n");
-	for(int idx = 0x0; idx < n; idx++){
+#ifdef DEBUG
+	printf("Sorted(QuickSort)\n");
+	for(int idx = 0x0; idx < vector_len; idx++){
 		Vector *p_in = p_list_in[idx];
-		for(int i = 0; i < m; i++)
-			printf("[%d] ", p_in->pdata[i]);
+		printf("[ ");
+		for(int i = 0; i < cell_len; i++)
+			printf(" %d ", p_in->pdata[i]);
+		printf(" ]");
 		printf("\n");
 	}
-	t_print(&p_merge_sort->p_counter->timer, "Time took for MergeSort");
+#endif
 
+	t_print(&p_sorter->p_counter->timer, "Time took for QuickSort");
+	printf("Number of comparisions: %ld \n", p_sorter->p_counter->counter);
+
+	del_sorter(p_sorter);
+	del_vector(p_list_in, vector_len);
 
 
 	return EXIT_SUCCESS;
